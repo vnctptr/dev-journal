3caec424aee1a96458ce88ba6010d87f995f7fd9
Vinnie Potrykus <vapotrykus@gmail.com>
2023-02-05 13:36:48 -0800

    Add sidebar to about, make note page pretty

--- dev-journal/app/controllers/home_controller.rb
+++ dev-journal/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @subjects = Subject.all
-    @notes = Note.order(:created).first(5)
+    @notes = Note.order('created DESC').first(5)
   end
 end
