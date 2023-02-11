65ad8ff224a031d41421690814c90558f60637f3
Vinnie Potrykus <vapotrykus@gmail.com>
2023-02-10 20:36:39 -0800

    Fix "About" path redirect

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   resources :subjects
   resources :notes
   get 'home/index'
-  get 'about/index'
+  get 'about', to: 'about#index'
   root 'home#index'
   get '/signin', to: 'sessions#new'
-  # get '/register', to: 'users#new'
-  # resources :users, only: [:create]
   resource :sessions, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
