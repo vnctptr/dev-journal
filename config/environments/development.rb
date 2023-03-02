ec7bfd1a3fd8020e5eb8877654611ebe9455dd38
Vinnie Potrykus <vapotrykus@gmail.com>
2023-03-02 15:44:50 -0800

    Remove ngrok address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  
-  config.hosts << "880d-207-6-151-236.ngrok.io"
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+    # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
