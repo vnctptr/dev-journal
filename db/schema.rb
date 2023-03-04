f2969449c322fd482a078270f22bea65c5899af0
Vinnie Potrykus <vapotrykus@gmail.com>
2023-03-04 12:14:44 -0800

    Create Tag scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_225542) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_200700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_225542) do
   end
 
   create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
