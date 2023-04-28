4028c4f3599fcb147fcb09722bac1c359866afd1
Vincent Potrykus <vapotrykus@gmail.com>
2023-04-27 17:16:56 -0700

    Add root path redirect

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   resource :sessions, only: [:create]
   get '/search', to: 'home#search'
+  match "*path", to: "home#index", via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
